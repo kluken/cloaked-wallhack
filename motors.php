47f08a9574cdaa8b52c3f8d477f73630a52dd164
Kieran Luken <kieranluken@gmail.com>
2015-06-26 19:55:08 +1000

    Motor Page almost Completed
    create table storing all the data from the motor controller. Needs to
    have things removed as per Tony's comments

--- motors.php
+++ motors.php
@@ -16,9 +16,281 @@
    </p>
 </div>
 <div id="content">
-<p> SHOW ME THE CONTENT <br/></p>
+<p><br/>
+<table>
+	<tr>
+		<th>
+			DC Bus Amp Hours
+		</th>
+		<td>
+			test
+		</td>
+		<th>
+			Motor Slip Speed
+		</th>
+		<td>
+			test
+		</td>
+		<th>
+			Vehicle Velocity
+		</th>
+		<td>
+			Test
+		</td>
+		<th>
+			Motor Velocity
+		</th>
+		<td>
+			test
+		</td>
+	</tr>
+	<tr>
+		<th>
+			Bus Current
+		</th>
+		<td>
+			test
+		</td>
+		<th>
+			Bus Voltage
+		</th>
+		<td>
+			test
+		</td>
+		<th>
+			Phase B Current
+		</th>
+		<td>
+			test
+		</td>
+		<th>
+			Phase C Current
+		</th>
+		<td>
+			test
+		</td>
+	</tr>
+	<tr>
+		<th>
+			Motor Current d
+		</th>
+		<td> 
+			Test
+		</td>
+		<th>
+			Motor Current q
+		</th>
+		<td>
+			Test
+		</td>
+		<th>
+			Motor Voltage d
+		</th>
+		<td>
+			Test
+		</td>
+		<th>
+			Motor Voltage q
+		</th>
+		<td>
+			Test
+		</td>
+	</tr>
+	<tr>
+		<th>
+			Back EMF d
+		</th>
+		<td>
+			Test
+		</td>
+		<th>
+			Back EMF q
+		</th>
+		<td>
+			Test
+		</td>
+		<th>
+			IPM Heatsink Temp
+		</th>
+		<td>
+			Test
+		</td>
+		<th>
+			Motor Temp
+		</th>
+		<td>
+			Test
+		</td>
+	</tr>
+	<tr>
+		<th>
+			IPM DSP Board Temp
+		</th>
+		<td>
+			Test
+		</td>
+		<th>
+			15V Rail
+		</th>
+		<td>
+			Test
+		</td>
+		<th>
+			3.3V Rail
+		</th>
+		<td>
+			Test
+		</td>
+		<th>
+			1.9V Rail
+		</th>
+		<td>
+			Test
+		</td>
+	</tr>
+	<tr>
+		<th>
+			Receive/Transmit Errors
+		</th>
+		<td>
+			Test/Test
+		</td>
+		<th>
+			Odometer
+		</th>
+		<td>
+			Test
+		</td>
+		<th>
+			Motor Tritium ID
+		</th>
+		<td>
+			Test
+		</td>
+		<th>
+			Motor Serial Number
+		</th>
+		<td>
+			Test
+		</td>
+	</tr>
+	<tr>
+		<th colspan = "8">
+			Error Flags
+		</th>
+	</tr>
+	<tr>
+		<th>
+			Desaturation Fault
+		</th>
+		<td id = "tableGood">
+			OK
+		</td>
+		<th>
+			15V Rail Under Voltage
+		</th>
+		<td id = "tableGood">
+			OK
+		</td>
+		<th>
+			Config Read Error
+		</th>
+		<td id = "tableGood">
+			OK
+		</td>
+		<th>
+			Watchdog Caused last Reset
+		</th>
+		<td id = "tableGood">
+			OK
+		</td>
+	</tr>
+	<tr>
+		<th>
+			Bad Motor Position Hall Sequence
+		</th>
+		<td id = "tableGood">
+			OK
+		</td>
+		<th>
+			DC Bus Over Volt
+		</th>
+		<td id = "tableGood">
+			OK
+		</td>
+		<th>
+			Software Over Current
+		</th>
+		<td id = "tableGood">
+			OK
+		</td>
+		<th>
+			Hardware Over Current
+		</th>
+		<td id = "tableGood">
+			OK
+		</td>
+	</tr>
+	<tr>
+		<th colspan = "8">
+			Limit Flags
+		</th>
+	</tr>
+	<tr>
+		<th>
+			Output Voltage PWM
+		</th>
+		<td id = "tableGood">
+			OK
+		</td>
+		<th>
+			Motor Current
+		</th>
+		<td id = "tableGood">
+			OK
+		</td>
+		<th>
+			Velocity
+		</th>
+		<td id = "tableGood">
+			OK
+		</td>
+		<th>
+			Bus Current
+		</th>
+		<td id = "tableGood">
+			OK
+		</td>
+	</tr>
+	<tr>
+		<th>
+			Bus Voltage Upper Limit
+		</th>
+		<td id = "tableGood">
+			OK
+		</td>
+		<th>
+			Bus Voltage Lower Limit
+		</th>
+		<td id = "tableGood">
+			OK
+		</td>
+		<th>
+			IPM Temp or Motor Temp
+		</th>
+		<td id = "tableGood">
+			OK
+		</td>
+		<th>
+		</th>
+		<td>
+		</td>
+	</tr>
+</table>
+</p>
 </div>
 </div>
 
 </body>
 </html> 
+

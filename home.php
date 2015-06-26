3d6d2ddf9092cca7c4b505fcbe88338c2a5ac152
Kieran Luken <kieranluken@gmail.com>
2015-06-26 17:28:33 +1000

    Base Files
    Base files with layout sorted

---
+++ home.php
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<title>UWS Solar Car Project - Home</title>
+	<link rel="stylesheet" href="assign.css" type="text/css" /> 
+	<!--<script type="text/JavaScript" src = "news.js"> </script>-->
+</head>
+<body>
+<div id="container">
+<div id="headerbar">
+   <h1>UWS Solar Car Project - Home</h1>
+   <p>Menu Options: <a href="home.php">Home</a> <a href="electrical.php">Electrical</a> <a href="motors.php">Motors</a> <a href="it.php">IT Admin</a>
+   </p>
+</div>
+<div id="content">
+<p> SHOW ME THE CONTENT <br/>
+</p>
+</div>
+</div>
+
+</body>
+</html> 

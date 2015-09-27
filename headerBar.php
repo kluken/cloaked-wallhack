13978487693bfc0207a8cd137a38cd3d0b57442e
Kieran Luken <kieranluken@gmail.com>
2015-09-27 17:25:22 +1000

    Remove dud link
    Remove dud link

--- headerBar.php
+++ headerBar.php
@@ -3,7 +3,7 @@
 		echo '<div id="headerbar">';
 			echo "<h1>UWS Solar Car Project - Home</h1>";
 			echo "<p>";
-				echo 'Menu Options: <a href="home.php">Home</a> <a href="electrical.php">Electrical</a> <a href="battery.php">Battery</a> <a href="motors.php">Motors</a> <a href="graphs.php">Graphs</a> <a href="rpi.php">IT Admin</a> <a href="database.php">Database</a> <a href="graphs.old.php">Old Graphs - May not work</a> <a href="strategy.php">Race Strategy</a>';
+				echo 'Menu Options: <a href="home.php">Home</a> <a href="electrical.php">Electrical</a> <a href="battery.php">Battery</a> <a href="motors.php">Motors</a> <a href="graphs.php">Graphs</a> <a href="rpi.php">IT Admin</a> <a href="database.php">Database</a> <a href="strategy.php">Race Strategy</a>';
 			echo "</p>";
 		echo "</div>";
 		?>
